c91597bae8930b36da12dfa1e534b1ce1fa83523
JulioSalazar <julio522002@gmail.com>
2022-10-18 19:29:30 -0300

    register working and use of crispy forms

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,7 @@
 from turtle import home
 from django.shortcuts import render
 
+
+
 def home(request):
     return render(request, 'blog_app/home.html')
--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import *
+from .models import *
 
-# Create your views here.
+
+
+def login_view(request):
+    pass
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data.get("username")
+            form.save()
+            context = {
+                "message" : f"Welcome, {user}!",
+                "form" : form,
+            }
+            return render(request, 'blog_app/home.html', context)
+        context = {
+            "error" : "Invalid data",
+            "form" : form,
+        }
+        return render(request, 'user_app/register.html', context)
+    
+    else:
+        context = {
+            "form" : UserRegisterForm()
+        }
+        return render(request, 'user_app/register.html', context)
+
+
+class Logout(LoginRequiredMixin, LogoutView):
+    template_name = "blog_app/home.html"
+    extra_context = {"message" : "You've successfully Logout"}
+
+
+def profile(request):
+    pass
+
+
+def profile_edit(request):
+    pass
+
+
+def get_avatar(request):
+    pass
