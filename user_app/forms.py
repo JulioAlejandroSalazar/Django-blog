ac2d2baeb6f08e490dc785283ddcb226263bbb48
JulioSalazar <julio522002@gmail.com>
2022-11-11 12:28:36 -0300

    birth date field now uses a calendar, fixed larged profile pics but now they appear distorted

--- user_app/forms.py
+++ user_app/forms.py
@@ -19,7 +19,7 @@ class UserRegisterForm(UserCreationForm):
 
 class UserEditForm(forms.ModelForm):
     username = forms.CharField(max_length=100, required=True, widget=forms.TextInput(attrs={"class":"form-control"}))
-    email = forms.EmailField(required=True, widget=forms.TextInput(attrs={"class":"form-control"}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={"class":"form-control"}))
     first_name = forms.CharField(max_length=100, label="Name", required=False, widget=forms.TextInput(attrs={"class":"form-control"}))
     last_name = forms.CharField(max_length=100, label="Last Name", required=False, widget=forms.TextInput(attrs={"class":"form-control"}))
 
@@ -28,18 +28,8 @@ class UserEditForm(forms.ModelForm):
         fields = ["username", "email", "first_name", "last_name"]
 
 
-class UserPasswordChangeForm(PasswordChangeForm):
-    old_password = forms.CharField(label="Old Password", widget=forms.PasswordInput(attrs={"class":"form-control"}))
-    new_password1 = forms.CharField(label="Old Password", widget=forms.PasswordInput(attrs={"class":"form-control"}))
-    new_password2 = forms.CharField(label="Old Password", widget=forms.PasswordInput(attrs={"class":"form-control"}))
-
-    class Meta:
-        model = User
-        fields = ["old_password", "new_password1", "new_password2"]
-        help_texts = {k:"" for k in fields}
-
-
 class ProfileEditForm(forms.ModelForm):
+    birth_date = forms.DateField(input_formats=['%d/%m/%Y', '%Y-%m-%d'], required=False, widget=forms.DateInput(attrs={'class': 'form-control'}))
     class Meta:
         model = Profile
         fields = '__all__'
@@ -48,11 +38,23 @@ class ProfileEditForm(forms.ModelForm):
         widgets = {
             'phone': forms.TextInput(attrs={'class': 'form-control'}),
             'country': forms.TextInput(attrs={'class': 'form-control'}),
-            'birth_date': forms.TextInput(attrs={'class': 'form-control'}),
+            #'birth_date': forms.DateInput(attrs={'class': 'form-control'}), had tu use it above, idk how to make it properly work in both ways
             'short_description': forms.TextInput(attrs={'class': 'form-control'}),
-            'generic_link': forms.TextInput(attrs={'class': 'form-control'}),
-            'github_link': forms.TextInput(attrs={'class': 'form-control'}),
-            'instagram_link': forms.TextInput(attrs={'class': 'form-control'}),
-            'facebook_link': forms.TextInput(attrs={'class': 'form-control'}),
+            'generic_link': forms.URLInput(attrs={'class': 'form-control'}),
+            'github_link': forms.URLInput(attrs={'class': 'form-control'}),
+            'instagram_link': forms.URLInput(attrs={'class': 'form-control'}),
+            'facebook_link': forms.URLInput(attrs={'class': 'form-control'}),
             'bio': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+
+class UserPasswordChangeForm(PasswordChangeForm):
+    old_password = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
+    new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={"class":"form-control"}))
+
+    class Meta:
+        model = User
+        fields = ["old_password", "new_password1", "new_password2"]
+        help_texts = {k:"" for k in fields}
+
--- user_app/models.py
+++ user_app/models.py
@@ -5,15 +5,15 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=100, blank=True, default="")
-    country = models.CharField(max_length=100, blank=True, default="")
+    phone = models.CharField(max_length=100, blank=True, null=True)
+    country = models.CharField(max_length=100, blank=True, null=True)
     birth_date = models.DateField(blank=True, null=True)
-    short_description = models.CharField(max_length=100, blank=True, default="")
-    generic_link = models.CharField(max_length=500, blank=True, default="")
-    github_link = models.CharField(max_length=200, blank=True, default="")
-    instagram_link = models.CharField(max_length=200, blank=True, default="")
-    facebook_link = models.CharField(max_length=200, blank=True, default="")
-    bio = models.TextField(blank=True, default="")
+    short_description = models.CharField(max_length=100, blank=True, null=True)
+    generic_link = models.CharField(max_length=500, blank=True, null=True)
+    github_link = models.CharField(max_length=200, blank=True, null=True)
+    instagram_link = models.CharField(max_length=200, blank=True, null=True)
+    facebook_link = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
     profile_pic = models.ImageField(blank=True, null=True, upload_to="profile_pic", default="profile_pic/default_profile_pic.png")
 
     def __str__(self):
