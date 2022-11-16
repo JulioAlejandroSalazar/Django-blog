9807bf280a7cb97395cfd68a393f28c9fe92c177
JulioSalazar <julio522002@gmail.com>
2022-11-16 17:33:25 -0300

    added counter to posts, updated profile and now posts show at home, not good looking

--- blog_app/views.py
+++ blog_app/views.py
@@ -8,7 +8,13 @@ from user_app.models import Profile
 
 
 def home(request):
-    return render(request, 'blog_app/home.html')
+    posts = Post.objects.all()
+    if not posts.exists():
+        return render(request, 'blog_app/home.html')
+    context = {
+        'posts' : posts
+    }
+    return render(request, 'blog_app/home.html', context)
 
 
 @login_required
@@ -22,6 +28,7 @@ def create_post(request):
             instance.save()
             count = Profile.objects.get(user=user)
             count.post_count += 1
+            count.save()
             messages.success(request, 'Publication posted successfully!')
             return redirect('home')
         else:
