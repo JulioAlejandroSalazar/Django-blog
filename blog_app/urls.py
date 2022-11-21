58637d747a4d020edff187bf5da386508d41f445
JulioSalazar <julio522002@gmail.com>
2022-11-21 12:57:15 -0300

    Edit and delete view for posts added, edit view needs to save the date and specify that the post has been updated

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('create-post', views.create_post, name='create_post'),
     path('view-post-<id>', views.get_post, name='get_post'),
+    path('edit-post-<id>', views.edit_post, name='edit_post'),
+    path('delete-post-<id>', views.delete_post, name='delete_post'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -53,9 +53,44 @@ def get_post(request, id):
 
 @login_required
 def edit_post(request, id):
-    pass
+    user = request.user
+    post = Post.objects.get(id=id)
+    if request.user == post.author:
+        if request.method == 'POST':
+            form = PostForm(request.POST, request.FILES, instance=post)
+            if form.is_valid():
+                instance = form.save(commit=False)
+                instance.author = user
+                instance.save()
+                messages.success(request, 'Publication edited successfully!')
+                return redirect('home')
+            else:
+                messages.error(request, 'Invalid data')
+                return redirect('edit_post')
+
+        context = {
+            'form' : PostForm(instance=post),
+            'post' : post,
+        }
+        return render(request, 'blog_app/edit_post.html', context)
+    messages.error(request, 'user not authorized to do that')
+    return redirect('home')
 
 
 @login_required
 def delete_post(request, id):
-    pass
+    post = Post.objects.get(id=id)
+    user = request.user
+    if request.method == 'POST':
+        post.delete()
+        count = Profile.objects.get(user=user)
+        count.post_count -= 1
+        count.save()
+        messages.success(request, 'Publication deleted successfully!')
+        return redirect('home')
+
+    context = {
+        'post' : post,
+        'message' : 'Are your sure you want to delete this post?',
+    }
+    return render(request, 'blog_app/delete_post.html', context)
